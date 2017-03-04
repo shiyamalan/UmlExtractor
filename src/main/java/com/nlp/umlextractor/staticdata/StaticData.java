a52b714ca118341e77f5bf2c6311153647b664b3
Shiyamalan <rsrishiya@gmail.com>
2017-03-04 10:49:36 +0530

    removed unwanted variables

--- src/main/java/com/nlp/umlextractor/staticdata/StaticData.java
+++ src/main/java/com/nlp/umlextractor/staticdata/StaticData.java
@@ -1,10 +1,6 @@
 package com.nlp.umlextractor.staticdata;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.nlp.umlextractor.model.Dependencies;
 import com.nlp.umlextractor.model.ModelData;
@@ -73,29 +69,6 @@ public class StaticData
 
   public static List<Dependencies> depencyList;
 
-  public static String umlFilePath = "";
 
-  public static String sourceFilePath = "";
-
-  public static String requirementFilePath = "";
-
-  public static String workspace = System.getProperty("user.home") + File.separator + "SATAnalyzer" + File.separator
-      + "Workspace" + File.separator;
-
-  public static String rootPathName; //property file it is the project root directory
-
-  public static HashMap<String, List<String>> folderNames = new HashMap<String, List<String>>();//project folder list
-
-  public static Set<String> paths = new HashSet<String>();
-
-  public static HashMap<String, List<String>> internalfileNames = new HashMap<String, List<String>>();//project folder's internal file list
-
-  /*
-  tool tip text of windows 
-  */
-  public static final String PROJECT_WINDOW_TOOL_TIP = "It contains project folders\n"
-      + "you can open using double press on that folder";
-
-  public static String statusString = "Comparation Started for Requirement and UML" + " \n Please wait";
 
 }
